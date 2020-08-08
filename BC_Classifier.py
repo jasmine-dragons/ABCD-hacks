78a46fa4edbc5f36001c5f576d3ed6dda4f2cde3
Pranav Daggubati <pranavdaggubati@gmail.com>
2020-08-08 10:21:16 -0700

    Update BC_Classifier.py

--- BC_Classifier.py
+++ BC_Classifier.py
@@ -4,31 +4,37 @@ from sklearn import neighbors, preprocessing
 from sklearn.model_selection import train_test_split
 
 def get_input(lst):
-     return lst
+     return np.array(lst)
 
-accuracies = []
-for i in range(100):
-    dframe = pd.read_csv('bcdata.csv')
-    dframe.replace('?', -99999, inplace=True)
-    dframe.drop(['id'], 1, inplace=True)
+def classify(data):
+    accuracies = []
+    for i in range(100):
+        dframe = pd.read_csv('bcdata.csv')
+        dframe.replace('?', -99999, inplace=True)
+        dframe.drop(['id'], 1, inplace=True)
 
-    X = np.array(dframe.drop(['class'],1))
-    y = np.array(dframe['class'])
+        X = np.array(dframe.drop(['class'],1))
+        y = np.array(dframe['class'])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    clf = neighbors.KNeighborsClassifier()
-    clf.fit(X_train, y_train)
+        clf = neighbors.KNeighborsClassifier()
+        clf.fit(X_train, y_train)
 
-    acc = clf.score(X_test, y_test)
-    accuracies.append(acc)
+        acc = clf.score(X_test, y_test)
+        accuracies.append(acc)
+    
+    avg_acc = sum(accuracies)/len(accuracies)
+    pred = clf.predict(get_input(data))
+    return avg_acc, pred
 
-example_measures = np.array([
+example_measures = [
     [4,2,1,1,1,2,3,2,1],
     [4,2,1,2,2,2,3,2,1],
     [4,2,1,2,2,2,3,2,1],
     [4,2,1,2,2,2,3,2,1]
-])
-pred = clf.predict(example_measures)
+]
+
+avg_acc, pred = classify(example_measures)
 print('Prediction:',pred)
-print(sum(accuracies)/len(accuracies))
+print('Accuracy:', avg_acc)
