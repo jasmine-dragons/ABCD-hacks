8bae508e1a0e768418a4c820adad521fa058fbd6
Aiden Szeto <66970460+aidenszeto@users.noreply.github.com>
2020-08-08 15:52:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route('/results')
-def results():
+def getValues():
     ClumpThickness = request.args.get('Clump Thickness')
     UniformityofCellSize = request.args.get('Uniformity of Cell Size')
     UniformityofCellShape = request.args.get('Uniformity of Cell Shape')
@@ -25,52 +23,3 @@ def results():
     NormalNucleoli = request.args.get('Normal Nucleoli')
     Mitoses = request.args.get('Mitoses')
     values = [ClumpThickness, UniformityofCellSize, UniformityofCellSize, MarginalAdhesion, SingleEpithelialCellSize, BareNuclei, BlandChromatin, NormalNucleoli, Mitoses]
-    
-    categories = ["Clump Thickness",
-    "Uniformity of Cell Size",
-    "Uniformity of Cell Shape",
-    "Marginal Adhesion",
-    "Single Epithelial Cell Size",
-    "Bare Nuclei",
-    "Bland Chromatin",
-    "Normal Nucleoli",
-    "Mitoses",
-    ]
-    inputs = {}
-
-    accuracies = []
-    for i in range(100):
-        dframe = pd.read_csv('bcdata.csv')
-        dframe.replace('?', -99999, inplace=True)
-        dframe.drop(['id'], 1, inplace=True)
-
-        X = np.array(dframe.drop(['class'],1))
-        y = np.array(dframe['class'])
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-        clf = neighbors.KNeighborsClassifier()
-        clf.fit(X_train, y_train)
-
-        acc = clf.score(X_test, y_test)
-        accuracies.append(acc)
-
-    avg_acc = sum(accuracies)/len(accuracies)
-    pred = clf.predict(y)
-
-    y = [[int(i) for i in x]]
-
-    for i in range(len(y)):
-        for item in categories:
-            inputs[item] = y[0][i]
-            i+=1
-
-    print(f'| Prediction: {borm(pred.item())} \t|')
-    print(f'| Accuracy of dataset: {avg_acc*100:.3f}% \t|')
-
-    my_client = pymongo.MongoClient('mongodb+srv://xdhacks:applebanana@cluster0.s06sf.mongodb.net/xdhacks?retryWrites=true&w=majority')
-    my_db = my_client['xdhacks']
-    my_col = my_db['data']
-    my_col.insert_one(inputs)
-    
-    return render_template('results.html')
