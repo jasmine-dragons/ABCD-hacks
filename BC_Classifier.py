043e729e1b09eede6e411412077c2b9cd567a7f1
Pranav Daggubati <pranavdaggubati@gmail.com>
2020-08-08 14:11:08 -0700

    Update BC_Classifier.py

--- BC_Classifier.py
+++ BC_Classifier.py
@@ -3,11 +3,9 @@ import pandas as pd
 from sklearn import neighbors, preprocessing
 from sklearn.model_selection import train_test_split
 import pymongo
+import dns
 
-def get_input(lst):
-     return lst
-
-def classify(data, idnum):
+def classify(data):
     accuracies = []
     for i in range(100):
         dframe = pd.read_csv('bcdata.csv')
@@ -26,8 +24,8 @@ def classify(data, idnum):
         accuracies.append(acc)
     
     avg_acc = sum(accuracies)/len(accuracies)
-    pred = clf.predict(get_input(data))
-    return avg_acc, pred, idnum
+    pred = clf.predict(data)
+    return avg_acc, pred
 
 def borm(num):
     return 'Benign Cancer' if num == 2 else 'Malignant Cancer'
@@ -35,35 +33,36 @@ def borm(num):
 def add_to_db(inputs):
     my_client = pymongo.MongoClient('mongodb+srv://xdhacks:applebanana@cluster0.s06sf.mongodb.net/xdhacks?retryWrites=true&w=majority')
     my_db = my_client['xdhacks']
-    my_db.insert_one(inputs)
+    my_col = my_db['data']
+    my_col.insert_one(inputs)
 
 ## This dict need to be added to the database
-p_id = {}
-inputs = {
-    "Clump Thickness" : None,
-    "Uniformity of Cell Size" : None,
-    "Uniformity of Cell Shape" : None,
-    "Marginal Adhesion" : None,
-    "Single Epithelial Cell Size" : None,
-    "Bare Nuclei" : None,
-    "Bland Chromatin" : None,
-    "Normal Nucleoli" : None,
-    "Mitoses" : None,
-}
-'''
-We need the input as a comma separated string of 9 integers from 1-10
-'''
+categories = ["Clump Thickness",
+    "Uniformity of Cell Size",
+    "Uniformity of Cell Shape",
+    "Marginal Adhesion",
+    "Single Epithelial Cell Size",
+    "Bare Nuclei",
+    "Bland Chromatin",
+    "Normal Nucleoli",
+    "Mitoses",
+    ]
+inputs = {}
+
 x = input('| Enter the parameters: ').split(',')
 y = [[int(i) for i in x]]
-
+print(y, y[0][1])
 for i in range(len(y)):
-    for item in inputs:
-        inputs[item] = y[i]
+    for item in categories:
+        inputs[item] = y[0][i]
+        i+=1
 
-avg_acc, pred, idnum = classify(y, 111111)
+avg_acc, pred = classify(y)
+
+inputs["Prediction"] = borm(pred.item())
+inputs["Accuracy"] = avg_acc*100
 
 print(f'| Prediction: {borm(pred.item())} \t|')
 print(f'| Accuracy of dataset: {avg_acc*100:.3f}% \t|')
-print(p_id)
 
 add_to_db(inputs)
