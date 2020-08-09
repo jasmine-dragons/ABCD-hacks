01a81ccdf040a2557ef5cfcd677027e69e7235f1
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-08-08 18:22:55 -0700

    test

--- pranav.py
+++ pranav.py
@@ -6,6 +6,6 @@ from bson.objectid import ObjectId
 my_client = pymongo.MongoClient('mongodb+srv://xdhacks:applebanana@cluster0.s06sf.mongodb.net/xdhacks?retryWrites=true&w=majority')
 my_db = my_client['xdhacks']
 my_col = my_db['data']
-patient = my_col.find_one({'_id': str(id)})
+patient = my_col.find_one({'_id': ObjectId('5f2f1e3d218e81e41faf62d4')})
 entries = list(patient)
 print(patient)
